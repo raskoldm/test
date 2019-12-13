b1be833861cbe13b6a6501eea8a26e943cede6eb
raskoldm <melnikovxdima@gmail.com>
2019-12-13 10:24:29 +0400

    13.12.19 morning

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 window.onload = function(){ 
 
-document.getElementById('sites__container').onclick = function visibleForm(params) {
+//     document.getElementById('sites__container').onclick = function visibleForm(params) {
 
-    alert("hello function work");
+//     alert("hello function work");
 
-    var element = document.getElementById('new-here');
+//     var element = document.getElementById('new-here');
 
-    var toggleVisibility = function(element) {
-        if(element.style.display=='block'){
-            element.style.display='none';
-        } else {
-            element.style.display='block';
-        }
-    };
+//     var toggleVisibility = function(element) {
+//         if(element.style.display=='block'){
+//             element.style.display='none';
+//         } else {
+//             element.style.display='block';
+//         }
+//     };
 
-}
-
-
+// }
 
 }
+
+function myFunction() {
+    console.log('aaa');
+    var x = document.getElementById("myLinks");
+    console.log(x);
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
